14ca92420cc7cbf50c29af16923f0b9de7859d4f
belomarkovicdev <belomarkovic1@gmail.com>
2022-11-18 18:53:48 +0100

    follow feature

--- src/app/pages/profile/profile.component.ts
+++ src/app/pages/profile/profile.component.ts
@@ -3,7 +3,7 @@ import { ActivatedRoute } from '@angular/router';
 import { tap } from 'rxjs';
 import { ERole } from 'src/app/model/ERole.model';
 import { Tweet } from 'src/app/model/Tweet.model';
-import { User } from 'src/app/model/User.model';
+import { Follow, User } from 'src/app/model/User.model';
 import { ProfileService } from 'src/app/services/profile.service';
 import { JwtUtilsService } from 'src/app/services/security/jwt-utils.service';
 import { TweetService } from 'src/app/services/tweet.service';
@@ -15,27 +15,53 @@ import { TweetService } from 'src/app/services/tweet.service';
 })
 export class ProfileComponent implements OnInit {
   user: User = new User('username', '', 'email@example.com', ERole.REGULAR_USER, false);
+  followers: User[] = [];
+  following: User[] = [];
   tweets!: Tweet[]
-  constructor(private profileService:ProfileService, private tweetService: TweetService, private route:ActivatedRoute) { }
+  constructor(private profileService: ProfileService, private tweetService: TweetService, private route: ActivatedRoute) { }
 
   ngOnInit(): void {
-    this.loadProfile()
-  }
-  addTweet(tweet: Tweet) {
-    this.tweetService.addTweetToTweets(this.tweets,tweet)
-  }
-  loadProfile(){
     this.route.params.subscribe(param => {
-      this.getTweets(param['username'])
       this.profileService.getUser(param['username']).subscribe(res => {
         this.user = res as User;
-      }
-      )
+        this.loadProfile()
+      })
     })
   }
-  getTweets(username:string){
+  addTweet(tweet: Tweet) {
+    this.tweetService.addTweetToTweets(this.tweets, tweet)
+  }
+  loadProfile() {
+    this.getTweets(this.username)
+    this.getFollowers(this.username)
+    this.getFollowing(this.username)
+  }
+  getTweets(username: string) {
     this.tweetService.getTweets(username).subscribe(res => {
       this.tweets = res as Tweet[]
     })
+  }
+  getFollowers(username: string) {
+    this.profileService.getFollowers(username).subscribe(res => {
+      this.followers = res as User[]
+    })
+  }
+  getFollowing(username: string) {
+    this.profileService.getFollowing(username).subscribe(res => {
+      this.following = res as User[]
+    })
+  }
+  doFollow(){
+    this.profileService.doFollow(this.username).subscribe(res => {
+      alert('Followed')
+    })
+  }
+  doUnfollow(){
+    this.profileService.doUnfollow(this.username).subscribe(res => {
+      alert('Unfollowed')
+    })
+  }
+  get username() {
+    return this.user.username
   }
 }
--- src/app/services/profile.service.ts
+++ src/app/services/profile.service.ts
@@ -1,6 +1,7 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { environment } from 'src/environments/environment.prod';
+import { Follow } from '../model/User.model';
 import { JwtUtilsService } from './security/jwt-utils.service';
 
 @Injectable({
@@ -21,5 +22,19 @@ export class ProfileService {
   }
   getUser(username: string) {
     return this.http.get(`${environment.api}/profile/users/${username}/`)
+  }
+  getFollowing(username: string) {
+    return this.http.get(`${environment.api}/social-graph/following/${username}/`)
+  }
+  getFollowers(username: string) {
+    return this.http.get(`${environment.api}/social-graph/followers/${username}/`)
+  }
+  doFollow(username: string) {
+    let follows: Follow = new Follow(this.jwtUtilsService.getUsername(), username)
+    return this.http.post(`${environment.api}/social-graph/follows/`, follows)
+  }
+  doUnfollow(username: string) {
+    let follows: Follow = new Follow(this.jwtUtilsService.getUsername(), username)
+    return this.http.delete(`${environment.api}/social-graph/follows/`, { body: follows })
   }
 }
